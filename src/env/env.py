8767d0c4c97704cb49ad74a61e2c29cd4255d0aa
karlxing <jinweixing1006@gmail.com>
2022-03-10 16:17:32 -0800

    add env

---
+++ src/env/env.py
@@ -0,0 +1,50 @@
+import imp
+import dmc2gym
+import gym
+import numpy as np
+from collections import deque
+
+def make_envs(args):
+    env = dmc2gym.make(
+        domain_name=args.domain_name,
+        task_name=args.task_name,
+        seed=args.seed,
+        visualize_reward=False,
+        from_pixels=True,
+        height=args.env_image_size,
+        width=args.env_image_size,
+        frame_skip=args.action_repeat
+    )
+    env.seed(args.seed)
+    env = FrameStack(env, k=args.frame_stack)
+    return env
+
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k):
+        gym.Wrapper.__init__(self, env)
+        self._k = k
+        self._frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=1,
+            shape=((shp[0] * k,) + shp[1:]),
+            dtype=env.observation_space.dtype
+        )
+        self._max_episode_steps = env._max_episode_steps
+
+    def reset(self):
+        obs = self.env.reset()
+        for _ in range(self._k):
+            self._frames.append(obs)
+        return self._get_obs()
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self._frames.append(obs)
+        return self._get_obs(), reward, done, info
+
+    def _get_obs(self):
+        assert len(self._frames) == self._k
+        return np.concatenate(list(self._frames), axis=0)
