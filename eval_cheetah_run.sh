d247887f0645e1be9e7b02664d3bcf73683d7131
karlxing <jinweixing1006@gmail.com>
2022-03-10 19:17:56 -0800

    update eval

--- eval_cheetah_run.sh
+++ eval_cheetah_run.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
 (python src/train.py  --agent sac    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 1) &
-(python src/train.py  --agent sac    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 2) &
-(python src/train.py  --agent sac    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 3) &
 wait;
 
 (python src/train.py  --agent sacae  --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 1) &
-(python src/train.py  --agent sacae  --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 2) &
-(python src/train.py  --agent sacae  --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 3) &
 wait;
 
 (python src/train.py  --agent curl   --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 1) &
-(python src/train.py  --agent curl   --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 2) &
-(python src/train.py  --agent curl   --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 3) &
 wait;
 
 (python src/train.py  --agent rad    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 1) &
-(python src/train.py  --agent rad    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 2) &
-(python src/train.py  --agent rad    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 3) &
 wait;
 
 (python src/train.py  --agent drq    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 1) &
-(python src/train.py  --agent drq    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 2) &
-(python src/train.py  --agent drq    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 3) &
 wait;
 
 (python src/train.py  --agent atc    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 1) &
-(python src/train.py  --agent atc    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 2) &
-(python src/train.py  --agent atc    --domain_name cheetah  --task_name run  --num_train_steps 250000   --action_repeat 4  --batch_size  512   --seed 3) &
 wait;
