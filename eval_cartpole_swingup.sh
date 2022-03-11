4c990cdd2b7999b4fe83aa142af26c264bbd7c54
karlxing <jinweixing1006@gmail.com>
2022-03-10 17:08:27 -0800

    fix eval

--- eval_cartpole_swingup.sh
+++ eval_cartpole_swingup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-(python src/train.py  --agent sac    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 1) &
-(python src/train.py  --agent sac    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 2) &
-(python src/train.py  --agent sac    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 3) &
+(python src/train.py  --agent sac    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 1) &
+(python src/train.py  --agent sac    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 2) &
+(python src/train.py  --agent sac    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 3) &
 wait;
 
-(python src/train.py  --agent sacae  --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 1) &
-(python src/train.py  --agent sacae  --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 2) &
-(python src/train.py  --agent sacae  --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 3) &
+(python src/train.py  --agent sacae  --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 1) &
+(python src/train.py  --agent sacae  --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 2) &
+(python src/train.py  --agent sacae  --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 3) &
 wait;
 
-(python src/train.py  --agent curl   --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 1) &
-(python src/train.py  --agent curl   --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 2) &
-(python src/train.py  --agent curl   --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 3) &
+(python src/train.py  --agent curl   --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 1) &
+(python src/train.py  --agent curl   --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 2) &
+(python src/train.py  --agent curl   --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 3) &
 wait;
 
-(python src/train.py  --agent rad    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 1) &
-(python src/train.py  --agent rad    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 2) &
-(python src/train.py  --agent rad    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 3) &
+(python src/train.py  --agent rad    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 1) &
+(python src/train.py  --agent rad    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 2) &
+(python src/train.py  --agent rad    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 3) &
 wait;
 
-(python src/train.py  --agent drq    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 1) &
-(python src/train.py  --agent drq    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 2) &
-(python src/train.py  --agent drq    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 3) &
+(python src/train.py  --agent drq    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 1) &
+(python src/train.py  --agent drq    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 2) &
+(python src/train.py  --agent drq    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 3) &
 wait;
 
-(python src/train.py  --agent atc    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 1) &
-(python src/train.py  --agent atc    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 2) &
-(python src/train.py  --agent atc    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action-repeat 8  --seed 3) &
+(python src/train.py  --agent atc    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 1) &
+(python src/train.py  --agent atc    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 2) &
+(python src/train.py  --agent atc    --domain_name cartpole  --task_name swingup  --num_train_steps 25000   --action_repeat 8  --seed 3) &
 wait;
